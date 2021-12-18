08b5fa474b631bc6a76461e3e44e50cd647ed7db
JOE_NB <dxgundam149@msn.com>
2021-12-18 14:39:13 +0800

    flask編輯中

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,5 +10,12 @@ def hello():
     return "Hello"
 
 
+@app.route("/get_data", methods=['GET', 'POST'])
+def getData():
+    if request.method == "POST":
+        print(request.values)
+    return render_template("main2.html")
+
+
 if __name__ == "__main__":
-    app.run('0.0.0.0', 8888, False)
+    app.run('0.0.0.0', 8888, True)
